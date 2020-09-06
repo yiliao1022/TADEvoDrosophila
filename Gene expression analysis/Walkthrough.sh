91f9b0f6e2361b6d89cdde06e865216a5f7c2998
yiliao1022 <34287238+yiliao1022@users.noreply.github.com>
2020-09-05 21:41:05 -0700

    Update Walkthrough.sh

--- Gene expression analysis/Walkthrough.sh
+++ Gene expression analysis/Walkthrough.sh
@@ -1,5 +1,4 @@
 ### Comparsion of gene expression profile between genes that inside and outside the TADs
-
 bedtools intersect -wa -a Dmel.Dpse.ortho.geneExp.dat -b BG3.domains.sort.bed S2.domains.sort.bed Kc167.domains.sort.bed -f 1 | sort -k1,1 -k2,2n | uniq > inside.dat
 cat inside.dat | wc -l
 6549
@@ -18,7 +17,48 @@ bedtools intersect -wa -a Dmel.Dpse.ortho.geneExp.dat -b Kc167.domains.sort.merg
 cat inside.shared.dat | wc -l
 4275
 
-### Comparsion of gene expression profile between genes that in conserved or noncopnserved TADs between Dmel and Dpse
-
+### Comparsion of gene expression profile between genes that in conserved or nonconserved TADs between Dmel and Dpse
+export BED=S2.domains.sort.bed
+sh work.2dpse.coordinate.sh
+cat S2.domains.sort.bed.2DpseCordinate.bed.bed | awk '{if ($6-$5>0.67*($3-$2) && $6-$5<1.5*($3-$2)) {print $0}}' > S2.2Dpse.overlift.bed
+export BED=BG3.domains.sort.bed
+sh work.2dpse.coordinate.sh
+cat BG3.domains.sort.bed.2DpseCordinate.bed.bed | awk '{if ($6-$5>0.67*($3-$2) && $6-$5<1.5*($3-$2)) {print $0}}' > BG3.2Dpse.overlift.bed
+export BED=Kc167.domains.sort.bed
+sh work.2dpse.coordinate.sh
+cat Kc167.domains.sort.bed.2DpseCordinate.bed.bed | awk '{if ($6-$5>0.67*($3-$2) && $6-$5<1.5*($3-$2)) {print $0}}' > Kc167.2Dpse.overlift.bed
+bedtools intersect -wb -a dpse_5000_blocks.bedpe.bed.bed -b Kc167.2Dpse.overlift.bed BG3.2Dpse.overlift.bed S2.2Dpse.overlift.bed -f 0.8 -F 0.8 | cut -f21-23 | sort -k1,1 -k2,2n | uniq  > Dmel.3cells.conservedwithDpse.tad.domains.bed
+bedtools intersect -wa -a inside.dat -b Dmel.3cells.conservedwithDpse.tad.domains.bed -f 1 | sort | uniq > inside.conserved.dat
+cat inside.conserved.dat | wc -l
+1868
+bedtools intersect -wa -a inside.dat -b Dmel.3cells.conservedwithDpse.tad.domains.bed -v -f 1 | sort | uniq > inside.nonconserved.dat
+cat inside.nonconserved.dat | wc -l
+4681
 
 ### Comparsion of gene expression profile between genes that closely linked (within 10kb) to conserved TAD boudnaries or nonconserved TAD boundaries
+for i in Kc167 BG3 S2; do cat $i.domains.sort.bed | awk '{print $1"\t"$2-5000"\t"$2+5000"\n"$1"\t"$2-5000"\t"$2+5000}'| sort -k1,1 -k2,2n | uniq > $i.domains.sort.boundaries.bed;done
+cat dpse_5000_blocks.bedpe.bed.bed | awk '{print $1"\t"$2-5000"\t"$2+5000"\n"$1"\t"$2-5000"\t"$2+5000}'| sort -k1,1 -k2,2n | uniq > dpse.domains.sort.boundaries.bed
+for i in Kc167 BG3 S2; do export BED=$i.domains.sort.boundaries.bed; sh work.2dpse.coordinate.sh; cat $i.domains.sort.boundaries.bed.2DpseCordinate.bed.bed | awk '{if ($6-$5>0.5*($3-$2) && $6-$5<2*($3-$2)) {print $0}}' > $i.boundaries.2Dpse.overlift.bed;done
+cat Kc167.boundaries.2Dpse.overlift.bed BG3.boundaries.2Dpse.overlift.bed S2.boundaries.2Dpse.overlift.bed | cut -f4-6| sort -k1,1 -k2,2n | uniq > 3cells.boundaries.bed
+bedtools intersect -wa -wb -a dpse.domains.sort.boundaries.bed -b Kc167.boundaries.2Dpse.overlift.bed BG3.boundaries.2Dpse.overlift.bed S2.boundaries.2Dpse.overlift.bed | cut -f8-10 | sort -k1,1 -k2,2n | uniq > conserved.boundaries.bed
+cat conserved.boundaries.bed | wc -l
+296
+bedtools intersect -wa -a 3cells.boundaries.bed -b conserved.boundaries.bed -v > nonconserved.boundaries.bed
+cat nonconserved.boundaries.bed | wc -l
+608
+bedtools intersect -wa -a Dmel.Dpse.ortho.geneExp.dat -b nonconserved.boundaries.bed | sort -k1,1 -k2,2n | uniq > nonconserved.boundaries.dat
+cat nonconserved.boundaries.dat | wc -l
+1123
+bedtools intersect -wa -a Dmel.Dpse.ortho.geneExp.dat -b conserved.boundaries.bed | sort -k1,1 -k2,2n | uniq > conserved.boundaries.dat
+cat conserved.boundaries.dat | wc -l
+608
+
+### Comparsion of gene expression profile between genes that closely linked (within 10kb) to shared TAD boudnaries or specific TAD boundaries
+for i in Kc167 BG3 S2;do bedtools merge -i $i.domains.sort.boundaries.bed > $i.domains.sort.boundaries.merge.bed;done
+bedtools intersect -wa -a Dmel.Dpse.ortho.geneExp.dat -b Kc167.domains.sort.boundaries.merge.bed BG3.domains.sort.boundaries.merge.bed S2.domains.sort.boundaries.merge.bed | sort -k1,1 -k2,2n | uniq -c | awk '{if($1==1){print $0}}' > boundaries.specific.dat
+cat boundaries.specific.dat | wc -l
+1240
+bedtools intersect -wa -a Dmel.Dpse.ortho.geneExp.dat -b Kc167.domains.sort.boundaries.merge.bed BG3.domains.sort.boundaries.merge.bed S2.domains.sort.boundaries.merge.bed | sort -k1,1 -k2,2n | uniq -c | awk '{if($1>1){print $0}}' > boundaries.shared.dat
+cat boundaries.shared.dat | wc -l
+1284
+
