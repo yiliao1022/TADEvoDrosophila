6b90b399153fb85f7ab8d006c797fea314e91411
yiliao1022 <34287238+yiliao1022@users.noreply.github.com>
2020-12-13 19:58:15 -0800

    Update walkthrough.sh

--- 5_SVs_Calling/walkthrough.sh
+++ 5_SVs_Calling/walkthrough.sh
@@ -49,5 +49,135 @@ for i in A2 A5 A6 AB8 B2 B3 B4 B6; do mkdir $i; done
 cd A2 
 /data/users/liaoy12/Softwares/parallelLastz.pl --qfile /data/users/liaoy12/liaoy12/TAD/genomes/dmel.chr.fasta --tfile /data/users/liaoy12/liaoy12/TAD/genomes/A2.fa --cfile conf --speedup 8 --length 100                                               
  
+ ##########Step 4: Chain/Net/Synnet
+
+#### Run each chromosome separately
+#!/bin/bash
+#$ -N ChainNet
+#$ -q jje128
+#$ -m beas
+
+module load jje/kent/2014.02.19
+export REF=/data/users/liaoy12/liaoy12/SV2020/Drosophila/genomes/ISO1
+
+
+for i in A1 A2 A3 A4 A5 A6 A7 AB8 B1 B2 B3 B4 B6 ORE Dere Dmau Dyak Dsec Dsim; do 
+#for i in Dmau Dsec Dsim; do
+
+export Sam=/data/users/liaoy12/liaoy12/SV2020/Drosophila/genomes/$i
+
+for j in ./ISO2_$i\_ISO1.2L.lz; do 
+
+axtChain -linearGap=/share/jje/liaoy12/Date/paras/medium $j $REF.2bit $Sam.2bit $j.chain
+
+chainPreNet $j.chain $REF.sizes $Sam.sizes $j.chain.filter
+
+chainNet -minSpace=1 $j.chain.filter $REF.sizes $Sam.sizes $j.chain.filter.tnet $j.chain.filter.qnet
+
+netSyntenic $j.chain.filter.tnet $j.chain.filter.tnet.synnet
+
+perl ../../../../SVsFromGenomes/SynnetFilter.pl $j.chain.filter.tnet.synnet
+
+netToAxt $j.chain.filter.tnet.synnet.filter $j.chain.filter $REF.2bit $Sam.2bit $j.chain.filter.tnet.synnet.axt
+
+axtToMaf $j.chain.filter.tnet.synnet.axt $REF.sizes $Sam.sizes $j.chain.filter.tnet.synnet.axt.maf
+
+single_cov2 $j.chain.filter.tnet.synnet.axt.maf > ISO1.$i.sing.maf;done
+
+#rm $j.chain $j.chain.filter $j.chain.filter.tnet $j.chain.filter.qnet $j.chain.filter.tnet.synnet.axt $j.chain.filter.tnet.synnet.axt.maf; done
+
+done
+
+########Step5: Call SVs
+
+#!/bin/bash
+#$ -N SV
+#$ -q jje
+#$ -m beas
+
+export REF=/data/users/liaoy12/liaoy12/SV2020/Drosophila/genomes/ISO1
+
+for i in `ls *.synnet`; do
+
+cat $i | grep 'net\|^ fill\|^  gap' > synnet1.txt
+cat $i | grep 'net\|^  gap\|^   fill' > synnet2.txt
+cat $i | grep 'net\|^   fill\|^    gap' > synnet3.txt
+cat $i | grep 'net\|^    gap\|^     fill' > synnet4.txt
+cat $i | grep 'net\|top' > Net_top.txt
+
+sed -i 's/^ fill/fill/g' Net_top.txt
+sed -i 's/net/fillnet/g' synnet1.txt
+sed -i 's/^ fill/fill/g' synnet1.txt
+sed -i 's/^  gap/gap/g' synnet1.txt
+sed -i 's/net/gapnet/g' synnet2.txt
+sed -i 's/^  gap/gap/g' synnet2.txt
+sed -i 's/^   fill/fill/g' synnet2.txt
+sed -i 's/net/fillnet/g' synnet3.txt
+sed -i 's/^   fill/fill/g' synnet3.txt
+sed -i 's/^    gap/gap/g' synnet3.txt
+sed -i 's/net/gapnet/g' synnet4.txt
+sed -i 's/^    gap/gap/g' synnet4.txt
+sed -i 's/^     fill/fill/g' synnet4.txt
+
+perl ~/Pipeplines/Lastz_SV/SVcallerl2_NEW.pl -input synnet2.txt -output level2 -refseq $REF
+perl ~/Pipeplines/Lastz_SV/SVcallerl1_NEW.pl -input synnet3.txt -output level3 -refseq $REF
+perl ~/Pipeplines/Lastz_SV/SVcallerl2_NEW.pl -input synnet4.txt -output level4 -refseq $REF
+perl ~/Pipeplines/Lastz_SV/SVcallerl1_NEW.pl -input synnet1.txt -output level1 -refseq $REF
+
+perl ~/Pipeplines/Lastz_SV/filter_NEW.pl Net_top.txt level1.Complex.txt
+perl ~/Pipeplines/Lastz_SV/filter_NEW.pl Net_top.txt level1.Deletion.txt
+perl ~/Pipeplines/Lastz_SV/filter_NEW.pl Net_top.txt level2.CNV.txt
+perl ~/Pipeplines/Lastz_SV/filter_NEW.pl Net_top.txt level2.INV.txt
+perl ~/Pipeplines/Lastz_SV/filter_NEW.pl Net_top.txt level3.Complex.txt
+perl ~/Pipeplines/Lastz_SV/filter_NEW.pl Net_top.txt level3.Deletion.txt
+perl ~/Pipeplines/Lastz_SV/filter_NEW.pl Net_top.txt level4.CNV.txt
+perl ~/Pipeplines/Lastz_SV/filter_NEW.pl Net_top.txt level4.INV.txt
+
+cat level1.Complex.txt.synteny.out level3.Complex.txt.synteny.out > $i.complex.sv
+cat level1.Deletion.txt.synteny.out level3.Deletion.txt.synteny.out > $i.deletion.sv
+cat level2.CNV.txt.synteny.out level4.CNV.txt.synteny.out > $i.CNV.sv
+cat level2.INV.txt.synteny.out level4.INV.txt.synteny.out > $i.INV.sv
+
+sort -k1,1 -k2,2n $i.complex.sv > $i.complex.sort.sv
+sort -k1,1 -k2,2n $i.deletion.sv > $i.deletion.sort.sv
+sort -k1,1 -k2,2n $i.CNV.sv > $i.CNV.sort.sv
+sort -k1,1 -k2,2n $i.INV.sv > $i.INV.sort.sv
+
+rm $i.complex.sv $i.deletion.sv $i.CNV.sv $i.INV.sv
+rm *.txt *.out
+mkdir SV
+mv *.sv SV;
+done
+
+########Step 6: Genotyping 
+(1) genotyping deletions
+#!/bin/bash
+#$ -N RepeatMasker
+#$ -q jje128
+export Chr=3L
+cat *.deletion.sort.sv > Deletions.$Chr\.txt
+cat Deletions.$Chr\.txt | sort -k1,1 -k2,2n > Deletions.$Chr\.sort.txt
+maf_order ../TBA/tba.$Chr\.maf ISO1 A1 A2 A3 A4 A5 A6 A7 AB8 B1 B2 B3 B4 B6 ORE Dsec Dsim Dmau Dyak Dere > tba.$Chr\.order.maf
+cat tba.$Chr\.order.maf | grep "\#\|\.$Chr\|\.ut\|^$\|score" > tba.$Chr\.order.syn.maf
+cat Deletions.$Chr\.sort.txt | grep -v Dere | grep -v Dyak | cut -f1-3 | uniq > $Chr\.bed
+split $Chr\.bed -l 1000 sub$Chr
+cat 3L.bed | awk '$3-$2<20001' > 3L.20k.bed
+
+
+#!/bin/bash
+#$ -N map_sub
+#$ -q jje128,jje,ionode,pub64,epyc
+#####$ -m beas
+#$ -pe openmp 1
+#$ -t 1-131
+
+num=$SGE_TASK_ID
+ID=$(head -n $num subfile.lst | tail -n 1 )
+perl ../../bin/ParseMaf2fastaFinal2.pl --svraw $ID --assemblies genome.lst --singmaf ./sing -tbamaf tba.3L.order.syn.maf -outdir tmp_$ID -output $ID.
+frequency.bed --refseq ISO1.3L
+
+(2) genotyping insertions
+
+
 
 
